3af05eda506c51576f1284c750c06ebbc7a8341c
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-04-26 15:33:58 +1000

    Update LoadEnvironment.m

--- LoadEnvironment.m
+++ LoadEnvironment.m
@@ -4,10 +4,17 @@ classdef BasicObject
    properties
       location
       mesh
+      type %shape: rectangle, circular, cylindar 
+      location
+      dimensions
    end
    methods
-      function m = mesh(location, type) 
+      function m = mesh(location, dimensions, type) 
          %make some meshes for collision detection
+         
+      function move
+         %move the location and meshs to a new position
+         
       end
    end
 end
