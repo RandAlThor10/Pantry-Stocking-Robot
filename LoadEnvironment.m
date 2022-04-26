c715a231c70566e82c43c11b5401502288b04a22
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-04-26 15:19:44 +1000

    Create LoadEnvironment.m

---
+++ LoadEnvironment.m
@@ -0,0 +1,13 @@
+%generate all environment in classes 
+
+classdef BasicObject
+   properties
+      location
+      mesh
+   end
+   methods
+      function m = mesh(location, type) 
+         %make some meshes for collision detection
+      end
+   end
+end
