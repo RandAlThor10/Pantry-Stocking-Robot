d388fb5ef5bd87c579577bdf07076921039f7b9c
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-04-26 15:08:17 +1000

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,30 @@
 clc
 clear
+
+%Load robot
+
+LinearDobot()
+
+
+%Load the Environment
+
+LoadEnvironment()
+
+
 %run the robot
+
+while true
+
+%Look at GUI
+
+
+
+%Stock pantry
+
+
+
+%Move robot manually
+
+end
+
+%Show on GUI stopping robot then shutdown gui
