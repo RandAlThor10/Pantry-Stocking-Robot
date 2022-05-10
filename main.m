d71fee0aaf40283d1c14a78d310930fd39edfd39
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-05-10 13:35:44 +1000

    Started animate movement - Zac

--- main.m
+++ main.m
@@ -12,19 +12,32 @@ LoadEnvironment()
 
 
 %run the robot
+moving = false;
 
 while true
 
 %Look at GUI
 
-
+%DobotGUI()  %Ashish calls gui to check for input?
 
 %Stock pantry
+    if moving == true
+        Dobot.model.animate(Traj(1,:));
+        Traj(1,:) = [];
+    elseif moving == false 
+        %set xyz target, immediately above object to grasp
+        if Items == []
+            %no items left to move, return to default pose then stop moving
+        else
+            %move to the next location
+            [Traj, steps, target] =  PlanTraj(,,Dobot.model, Dobot.model.getpos) 
+        end
+    elseif moving == "manual"
+        
+    %Move robot manually
 
-
-
-%Move robot manually
-
+    end
 end
+
 
 %Show on GUI stopping robot then shutdown gui
