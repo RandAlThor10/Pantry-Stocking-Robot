ad2666bb8920f69b2b5f6267c3b43de16750f1e6
Benny <Benny@DESKTOP-69HRNP9.modem>
2022-05-10 18:04:58 +1000

    Update to Dobot Model, Joint Angles and Ikine function, Dobot can animate

--- DOBOTTest.m
+++ DOBOTTest.m
@@ -5,19 +5,141 @@ set(0,'DefaultFigureWindowStyle','docked')
 clc
 
 
+%% TEst
 
+UR5R = UR5;
+
+qr = [0 -pi/2 0 -pi/2 -pi 0];
+UR5R.model.offset = qr;
+UR5R.PlotAndColourRobot();
+
+
+
+
+%% Launch Dobot
+
+ close all
+
+set(0,'DefaultFigureWindowStyle','docked')
+clc
+
+Dobot = LinearDobot(false);
+
+qr = [ 0 0 0 0 0];
+
+qe = [deg2rad(0) deg2rad(-90) deg2rad(0) deg2rad(0) deg2rad(0)];
+
+Dobot.model.offset = qe;
+r_pose = Dobot.model.getpos()
+
+tr1 = zeros(4,4,Dobot.model.n);
+tr1(:,:,1) = Dobot.model.base;
+L = Dobot.model.links;
+for i = 1 : Dobot.model.n
+    tr1(:,:,i+1) = tr1(:,:,i) * trotz(qr(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+end
+
+Dobot.PlotAndColourRobot();
+
+   %% Dobot Stick Links (Not the same as the Model Link, Needs to be fixed to match)
+
+
+
+
+
+    L(1) = Link([0      0.138  0        -pi/2  0]);
+    L(2) = Link([0      0      0.135    0      0]);   
+    L(3) = Link([0      0      0.147    pi      0]);
+    L(4) = Link([0      0      0.075    pi/2   0]);
+    L(5) = Link([0      0.1     0        0      0]);
+
+    L(1).qlim = [-135 135]*pi/180;
+    L(2).qlim = [-5 85]*pi/180;
+    L(3).qlim = [-5 85]*pi/180;
+    L(4).qlim = [-90 90]*pi/180;
+    L(5).qlim = [-85 85]*pi/180;
+
+    L(1).offset = 0;
+    L(2).offset = -pi/2;
+
+    
+
+
+
+robot = SerialLink(L,'name','myrobot');
+% q = [deg2rad(45) deg2rad(q2) deg2rad(q3m) deg2rad(q4) deg2rad(q5)];     
+qr = [deg2rad(0) deg2rad(0) deg2rad(0) deg2rad(0) deg2rad(0)]; % Default Upright Pose
+
+
+tr = zeros(4,4,robot.n);
+tr(:,:,1) = robot.base;
+L = robot.links;
+for i = 1 : robot.n
+    tr(:,:,i+1) = tr(:,:,i) * trotz(qr(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+end
+
+
+
+robot.plot(qr);
+robot.fkine(qr)
+robot.teach;
+
+%% Testing Animation for model Dobot
+
+DOB_T1 = transl(0.21,-0.142,0.1);           % Generating T-matrix
+DOB_q1 = JPikine(DOB_T1);                   % Generating Joint states                                              % Joint Angles for Brick Pickup Location (Brick 1)  
+Trej1 = jtraj(Dobot.model.getpos,DOB_q1,200);    % Creating trajectory                                                                % Trajectory from Start to Pick-up Location
+
+
+
+
+% Animate to Location
+for i = 1:200
+    Dobot.model.animate(Trej1(i,:));
+    
+    DOB_tr1 = Dobot.model.fkine(Trej1(i,:));
+    
+    drawnow()
+end
+
+%% Testing Animation for stick Dobot
+
+
+
+DOB_T2 = transl(0.21,-0.142,0.1);         % Generating T-matrix
+DOB_q2 = JPikine(DOB_T2);                 % Generating Joint States                                                % Joint Angles for Brick Pickup Location (Brick 1)  
+Trej2 = jtraj(robot.getpos,DOB_q2,50);    % Creating trajectory                                                                  % Trajectory from Start to Pick-up Location
+
+
+
+
+% Animate to Location
+for i = 1:50
+    robot.animate(Trej2(i,:));
+    
+    DOB_tr2 = robot.fkine(Trej2(i,:));
+    
+    drawnow()
+end
 
 
  %% Ikine James Poon
+
+function [newQ] = JPikine(EEmatrix)
 
 
 a2 = 0.135;                % Length from Joint 2 to Joint 3
 a3 = 0.147;                % Length from Joint 3 to Joint 4
 
+EEx = EEmatrix(1,4);              % End Effector X position
+EEy = EEmatrix(2,4);              % End Effector Y position
+EEz = EEmatrix(3,4);              % End Effector Z position
 
-x = 0.17;                  % X location for Joint 4
-y = 0.0;                   % Y location for Joint 4
-z = 0.27 - 0.138 ;         % Z location for Joint 4
+
+
+x = EEx - 0.075*cos(acos(EEmatrix(1,1)));              % X location for Joint 4 from Joint 2
+y = EEy - 0.075*sin(acos(EEmatrix(1,1)));              % Y location for Joint 4 from Joint 2
+z = EEz + 0.1 - 0.138 ;                                % Z location for Joint 4 from Joint 2
 
 l = sqrt(x^2 + y^2);
 D = sqrt(l^2 + z^2);
@@ -35,143 +157,11 @@ q3m = 90 - q2 + q3r;
 q4 =  q3r;
 q5 = 0;
 
+newQ(1,1) = deg2rad(q1);
+newQ(1,2) = deg2rad(q2);
+newQ(1,3) = deg2rad(q3m);
+newQ(1,4) = deg2rad(q4);
+newQ(1,5) = deg2rad(q5);
 
-%% Launch Dobot
-
-close all
-
-set(0,'DefaultFigureWindowStyle','docked')
-clc
-
-R = LinearDobot(false);
-
-qr = [ 0 0 0 0 0];
-
-qe = [deg2rad(q1) deg2rad(q2) deg2rad(q3m) deg2rad(q4) deg2rad(q5)];
-
-R.model.offset = qe;
-r_pose = R.model.getpos()
-
-R.PlotAndColourRobot();
-
-
-
-
-
-
-
-% Dobot Model links
-%      L(1) = Link([0      0.138      0   -pi/2      0]);
-%      L(2) = Link([0      0      0.135     0      0]);
-%      L(3) = Link([0      0    0.147      pi    0]);
-%      L(4) = Link([0      0      0.05      pi/2  0]);
-%      L(5) = Link([0      0.1      0    0     0]);
-
-%% Testing Animation for model
-
-DOB_T1 = transl(0.2,0,0.1);  % Transform for Brick Location (Brick 1) 
-DOB_q1 = R.model.ikcon(DOB_T1,R.model.getpos());                                                                 % Joint Angles for Brick Pickup Location (Brick 1)  
-Trej = jtraj(R.model.getpos,DOB_q1,200);                                                                     % Trajectory from Start to Pick-up Location
-
-
-
-
-% Animate to Bricks' Location
-for i = 1:200
-    R.model.animate(Trej(i,:));
-    
-    DOB_tr = R.model.fkine(Trej(i,:));
-    
-    drawnow()
 end
-
-   %% Dobot Stick Links (Not the same as the Model Link, Needs to be fixed to match)
-
-close all
-
-
-
-    L(1) = Link([0      0.138  0        -pi/2  0]);
-    L(2) = Link([0      0      0.135    0      0]);   
-    L(3) = Link([0      0      0.147    pi      0]);
-    L(4) = Link([0      0      0.075    pi/2   0]);
-    L(5) = Link([0      0.1     0        0      0]);
-
-      L(1).qlim = [-135 135]*pi/180;
-      L(2).qlim = [5 80]*pi/180;
-      L(3).qlim = [15 170]*pi/180;
-      L(4).qlim = [-90 90]*pi/180;
-      L(5).qlim = [-85 85]*pi/180;
-
-      L(1).offset = 0;
-      L(2).offset = -pi/2;
-
-    
-
-
-
-robot = SerialLink(L,'name','myrobot');
-q = [deg2rad(q1) deg2rad(q2) deg2rad(q3m) deg2rad(q4) deg2rad(q5)];%     
-qr = [0 0 0 0 0];
-
-robot.plot(q);
-robot.fkine(q)
-robot.teach;
-
-%% Limit Test
-
- titleStr = {'Model limits given ACTUAL real robot limits','Model limits given SUGGESTED real robot limits'};
-
-  actualRealQ2lim = deg2rad([-5,85]);   
-  suggestedRealQ2lim = deg2rad([5,80]);  %> Joint 3: Actual vs Suggested real joint limits actual joint limits less than 0 (or more than 90) cause problems with IMU (officially q2limDegs = -10 to 95 )   
-  actualRealQ3lim = deg2rad([-10,95]);   
-  suggestedRealQ3lim = deg2rad([5,85]); 
-
-  
-    
-   
-   
-
- qlimActualAndSuggested = {R.model.qlim,R.model.qlim}; 
-  qlimActualAndSuggested{1}(2,:) = actualRealQ2lim; 
-  qlimActualAndSuggested{1}(3,:) = actualRealQ3lim; 
-  qlimActualAndSuggested{2}(2,:) = suggestedRealQ2lim; 
-  qlimActualAndSuggested{2}(3,:) = suggestedRealQ3lim;             
-                       
-  fig_h = figure; 
-  for limitsIndex = 1:size(titleStr,2) 
-    clf(fig_h); 
-    data = []; 
-    lowerLimit = []; 
-    upperLimit = []; 
-                 
-    qlim = qlimActualAndSuggested{ limitsIndex }; 
-                 
-    for q2 = qlim(2,1):0.01:qlim(2,2) 
-        for theta3 = qlim(3,1):0.01:qlim(3,2)+0.01 
-            q3 = pi/2 - q2 + theta3; 
-            data = [data;q2,q3]; %#ok<AGROW> 
-            if theta3 <= qlim(3,1) 
-                lowerLimit = [lowerLimit;q2,q3]; %#ok<AGROW> 
-            elseif qlim(3,2) <= theta3 
-                upperLimit = [upperLimit;q2,q3]; %#ok<AGROW> 
-            end 
-        end 
-    end
-     plot(rad2deg(data(:,1)),rad2deg(data(:,2)),'g.'); 
-    hold on; 
-    plot(rad2deg(lowerLimit(:,1)),rad2deg(lowerLimit(:,2)),'b'); 
-    plot(rad2deg(upperLimit(:,1)),rad2deg(upperLimit(:,2)),'r'); 
-    title(titleStr{limitsIndex}) 
-    set(gca,'fontSize',15) 
-    xlabel('q2') 
-    ylabel('q3')                 
-    grid on; 
-    axis([-10,90,-15,200]); 
-    drawnow(); 
-    img = getframe(gcf); 
-    imwrite(img.cdata, [titleStr{limitsIndex}, '.jpg']); 
-  end 
-  
-
  
--- Dobot_PLY_files/LinearDobot.m
+++ Dobot_PLY_files/LinearDobot.m
@@ -4,7 +4,7 @@ classdef LinearDobot < handle
         model;
         
         %>
-        workspace = [-2 2 -2 2 -0.3 2];   
+        workspace = [-1 1 -1 1 -0.3 1];   
         
         %> Flag to indicate if gripper is used
         useGripper = false;        
@@ -40,6 +40,13 @@ function GetDobot(self)
     L(5) = Link([0     0.1     0        0      0]);
 
 
+    % Test
+%     L(1) = Link([0      0      0        0      1]);
+%     L(2) = Link([0      0.138  0        -pi/2  0]);
+%     L(3) = Link([0      0      0.135    0      0]);   
+%     L(4) = Link([0      0      0.147    pi     0]);
+%     L(5) = Link([0      0      0.075    pi/2   0]);
+%     L(6) = Link([0     0.1     0        0      0]);
 
 
     % Incorporate joint limits
@@ -47,13 +54,20 @@ function GetDobot(self)
    
     L(1).qlim = [-135 135]*pi/180;
     L(2).qlim = [5 80]*pi/180;
-    L(3).qlim = [15 170]*pi/180;
+    L(3).qlim = [15 85]*pi/180;
     L(4).qlim = [-90 90]*pi/180;
     L(5).qlim = [-85 85]*pi/180;
- 
 
-     L(1).offset = 0;
-     L(2).offset = -pi/2;
+    % Test
+%     L(1).qlim = [-0.8 0];
+%     L(2).qlim = [-135 135]*pi/180;
+%     L(3).qlim = [5 80]*pi/180;
+%     L(4).qlim = [15 170]*pi/180;
+%     L(5).qlim = [-90 90]*pi/180;
+%     L(6).qlim = [-85 85]*pi/180;
+
+     L(2).offset = 0;
+     L(3).offset = -pi/2;
 
 
  
@@ -62,7 +76,9 @@ function GetDobot(self)
     self.model = SerialLink(L,'name',name);
   
     % Rotate robot to the correct orientation
-%           self.model.base = self.model.base * trotx(pi);
+
+%   self.model.base = self.model.base  * troty(-pi/2);
+
 end
 %% PlotAndColourRobot
 % Given a robot index, add the glyphs (vertices and faces) and
