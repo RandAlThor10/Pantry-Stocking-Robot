b4e94ea66fb9406969bcbaa36ec2d4cb23eb8913
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-05-11 12:23:18 +1000

    Update BasicObject.m

--- BasicObject.m
+++ BasicObject.m
@@ -8,10 +8,16 @@ classdef BasicObject
       type %Environment or pantry item
    end
    methods
+      function obj = BasicObject(val1, val2)
+          obj.location = val1;
+          obj.originvertices = val2;
+          obj.currentvertices = [obj.originvertices, ones(size(obj.originvertices,1),1)]*transl(obj.location)
+       end
+   
       function move(obj,[newx,newy,newz],rotz)
          %move the location and meshs to a new position
          obj.location = [newx,newy,newz]
-         obj.currentvertices = [originvertices, ones(size(originvertices,1),1)]*transl(newx,newy,newz)
+         obj.currentvertices = [obj.originvertices, ones(size(obj.originvertices,1),1)]*transl(newx,newy,newz)
       end
    end
 end
