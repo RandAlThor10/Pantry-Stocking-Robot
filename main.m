cc0ccfb59ad47ea87102bddab2b2fa4fc9cd06c9
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-05-16 17:20:01 +1000

    Logic for target item - Zac

--- main.m
+++ main.m
@@ -45,12 +45,11 @@ Box = BasicObject('Box.ply',[-0.15,0.25,0],0);
 
 Objarray = [Cerealbox, Shelf1, Shelf2, Fire_Extinguisher, SauceBottle, Barrier1 , Barrier2, Barrier3, Barrier4, Barrier5, Barrier6, Box];
 
-
 %run the robot
 moving = false;
 
 while any([Objarray.type]) %continue program until all objects are type 0
-
+    T = Dobot.model.fkine(Dobot.model.getpos);
 %Look at GUI
 
 %DobotGUI()  %Ashish calls gui to check for input?
@@ -61,17 +60,27 @@ while any([Objarray.type]) %continue program until all objects are type 0
         Traj(1,:) = [];
     elseif moving == false 
         %set xyz target, immediately above object to grasp
-        if Items == []
-            %no items left to move, return to default pose then stop moving
+        if any([Objarray.type])
+            %no items left to move, stop moving
         else
             %move to the next location
-            [Traj, steps, target] =  PlanTraj(Traj,Dobot.model, Dobot.model.getpos) 
+            for i = 1:size(Objarray,2) %find closest object to be moved
+                if Objarray(i).type == 0 %obj that doesn't need to be moved
+                    distance(i) = 9001;
+                else
+                    distance(i) = sqrt(sum((T(1:3,4)'-Objarray(i).location.^2)));
+                end
+            end
+            [~,targetindex] = min(distance);
+            [Traj, steps, target] =  PlanTraj(T(1:3,4)',target, Objarray(targetindex).location,Dobot.model, Dobot.model.getpos);
+            
         end
     elseif moving == "manual"
         
     %Move robot manually
 
     end
+  pause(0.001)  
 end
 
 
