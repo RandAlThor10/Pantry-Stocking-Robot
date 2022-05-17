3f9a53cce6cf0576cfda4269706894f71eea79d8
Zachary Francis <86774296+RandAlThor10@users.noreply.github.com>
2022-05-18 02:56:14 +1000

    Update PickAndPlace.m

--- PickAndPlace.m
+++ PickAndPlace.m
@@ -1,20 +1,18 @@
 function PickAndPlace(Dobot,Objarray)
 
-%run the robot
 moving = false;
 transporting = true;
-steps = 200;
+steps = 100;
 
 Lift = false;
 Waypoint = false;
 Endlocation = false;
-
+NextObj = false;
+cam=[];
+[cam,~] = DobotCam(1, Dobot.model, cam);
+pause(1)
 while any([Objarray.type]) %continue program until all objects are type 0
     T = Dobot.model.fkine(Dobot.model.getpos);
-%Look at GUI
-
-%DobotGUI()  %Ashish calls gui to check for input?
-
 %Stock pantry
     if moving == true
 
@@ -22,11 +20,20 @@ while any([Objarray.type]) %continue program until all objects are type 0
          % Animate to the object
          for x = 1:steps
             Dobot.model.animate(Traj(x,:));
+            [cam,warning] = DobotCam(0, Dobot.model, cam);
+            if warning
+                print("Warning sign!! Robot stopped for safety.")
+                return
+            end
+            if DobotCollision(Dobot.model,Objarray)
+                print("Collision!! Robot stopped for safety.")
+                return
+            end
             drawnow()
          end
          % Once complete create new traj for moving the obj
          Lift = true;
-         transporting = 1;
+         transporting = true;
 
       elseif transporting == true
 
@@ -52,6 +59,7 @@ while any([Objarray.type]) %continue program until all objects are type 0
           DOB_q = JPikine(DOB_T); 
           Traj = jtraj(Dobot.model.getpos,DOB_q,steps);
           Endlocation = false;
+          NextObj = true;
 
         end
 
@@ -61,24 +69,64 @@ while any([Objarray.type]) %continue program until all objects are type 0
        % Animate
        for x = 1:steps
             Dobot.model.animate(Traj(x,:));
+            [cam,warning] = DobotCam(0, Dobot.model, cam);
+            if warning
+                print("Warning sign!! Robot stopped for safety.")
+                return
+            end
+            if DobotCollision(Dobot.model,Objarray)
+                print("Collision!! Robot stopped for safety.")
+                return
+            end
             DOB_tr = Dobot.model.fkine(Traj(x,:));
             attach(Objarray(targetindex),DOB_tr);
             drawnow()
        end
         
       
+       if NextObj == true
+        transporting = false;
+        moving = false;
+        Objarray(targetindex).type = 0;                                      % Become Static
+        
+        % Move to Safe Pose
+
+        if targetindex == 2
+
+        DOB_T = transl(0.1,-0.18,0.125);                                        % Generating T-matrix
+        DOB_q = JPikine(DOB_T); 
+        Traj = jtraj(Dobot.model.getpos,DOB_q,steps);
+
+        else
+        DOB_T = transl(0.18,0.01,0.075);                                        % Generating T-matrix
+        DOB_q = JPikine(DOB_T); 
+        Traj = jtraj(Dobot.model.getpos,DOB_q,steps);
+
+        end
+
+        for x = 1:steps
+            Dobot.model.animate(Traj(x,:));
+            [cam,warning] = DobotCam(0, Dobot.model, cam);
+            if warning
+                print("Warning sign!! Robot stopped for safety.")
+                return
+            end
+            if DobotCollision(Dobot.model,Objarray)
+                print("Collision!! Robot stopped for safety.")
+                return
+            end
+            DOB_tr = Dobot.model.fkine(Traj(x,:));  
+            drawnow()
+        end
        
-       transporting = false;
-       moving = false;
-       Objarray(targetindex).type = 0;                                      % Become Static
-       
+       end
 
      end
        % move until finished
     elseif moving == false 
-        %set xyz target, immediately above object to grasp
+       
       
-            %move to the next object
+            % Move to the next object
             for i = 1:size(Objarray,2) %find closest object to be moved
                 if Objarray(i).type == 0 %obj that doesn't need to be moved
                     distance(i) = 9001;
@@ -88,7 +136,7 @@ while any([Objarray.type]) %continue program until all objects are type 0
             end
             [~,targetindex] = min(distance);
             % Found closest Obj
-            transporting = 0;
+            transporting = false;
             % Trajectory to the  Obj location
             
             currentT = Dobot.model.fkine(Dobot.model.getpos); 
@@ -98,13 +146,16 @@ while any([Objarray.type]) %continue program until all objects are type 0
             %[Traj, steps] =  PlanTraj(A(1:3,4)', Objarray(targetindex).location, Dobot.model, Dobot.model.getpos);
             
             Traj = jtraj(Dobot.model.getpos,DOB_q,steps);                      % Creating trajectory
-
+            
+            NextObj = false;
             moving = true;
+            
      
     elseif moving == "manual"
-        NextObj = false;
+        
     %Move robot manually
 
     end
   pause(0.001)  
+end
 end
